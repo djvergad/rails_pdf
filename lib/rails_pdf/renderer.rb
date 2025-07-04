98c1f2a2a1157b000cb5495b00b57068aecb721a
Dimitrios J. Vergados <djvergad@gmail.com>
2025-07-04 13:30:10 +0300

    To work with erb

--- lib/rails_pdf/renderer.rb
+++ lib/rails_pdf/renderer.rb
@@ -19,17 +19,24 @@ module RailsPDF
 
     def render(&block)
       controller = ActionController::Base.new
-      view = ActionView::Base.new(ActionController::Base.view_paths, {}, controller)
-      params = { file: @file, layout: @layout }
-      params = params.merge(locals: @locals) if @locals.present?
-      content = view.render(params)
+      view = ActionView::Base.with_empty_template_cache.new(ActionView::LookupContext.new(ActionController::Base.view_paths), {}, controller)
+
+      if @locals.present?
+        @locals.each do |key, value|
+          view.instance_variable_set("@#{key}", value)
+        end
+      end
+
+      template_path = @file # e.g., "app/pdf/collection_report/file.pug.erb"
+      erb_source = File.read(template_path)
+      content = ERB.new(erb_source).result(view.instance_eval { binding })
 
       logger.debug "RailsPDF ====="
       logger.debug "RailsPDF content:\n#{content}"
       logger.debug "RailsPDF ====="
 
       begin
-        input  = BetterTempfile.new("in.pug")
+        input = BetterTempfile.new("in.pug")
         output = BetterTempfile.new("out.pdf")
 
         input.write(content)
@@ -41,7 +48,7 @@ module RailsPDF
 
         err = Open3.popen3(*command) do |_stdin, _stdout, stderr|
           logger.debug _stdout.read
-          logger.debug '------'
+          logger.debug "------"
           logger.debug stderr.read
         end
 
@@ -59,7 +66,7 @@ module RailsPDF
 
     def render_to_file(path_and_filename = "report.pdf")
       render do |data|
-        File.open(path_and_filename, 'wb') do |f|
+        File.open(path_and_filename, "wb") do |f|
           f.write(data)
           f.close
         end
@@ -81,8 +88,5 @@ module RailsPDF
     def logger
       Rails.logger
     end
-
   end
 end
-
-
